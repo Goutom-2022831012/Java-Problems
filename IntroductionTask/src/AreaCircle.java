88553e2103e9b2927ff40537072f7ecae4afac18
Goutom Das Dipto <goutomdipto794@gmail.com>
2024-12-10 23:21:21 +0600

    Completed All Tasks

---
+++ IntroductionTask/src/AreaCircle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AreaCircle {
+    public static void main(String args[]) {
+        Scanner length = new Scanner(System.in);
+        System.out.println("Radius:");
+        float pi = 3.1426f;
+        float rad = length.nextFloat();
+        float area =  pi * (float) Math.pow(rad, 2);
+        System.out.println("Area: " + area);
+        length.close();
+    }
+}

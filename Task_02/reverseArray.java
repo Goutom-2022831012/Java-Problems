0f9d27b87144c7aeb6dfd418c713adda16afdc6c
Goutom Das Dipto <goutomdipto794@gmail.com>
2025-01-06 00:11:29 +0600

    more of task02

---
+++ Task_02/reverseArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class reverseArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+           arr[i] = input.nextInt();
+        }
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
+        }
+        System.out.println("The array in reverse order is:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        input.close();
+    }
+}
